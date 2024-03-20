45efdb378855a8e659948c9d0c2bdf55f2143c5a
Ankit Chandak <67820807+ankitdchandak@users.noreply.github.com>
2024-03-20 19:39:42 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -5,12 +5,4 @@ sudo apt update
 sudo apt install -y containerd.io
 
 
-containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
 
-sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-
-sudo systemctl restart containerd
-sudo systemctl enable containerd
-
-
-sudo apt-get update
