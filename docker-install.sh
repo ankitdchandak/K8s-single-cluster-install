5c286615df923117535bf706fc32127d3f48c31c
Ankit Chandak <67820807+ankitdchandak@users.noreply.github.com>
2024-03-20 19:34:22 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,34 +1,6 @@
-sudo apt update
-sudo apt upgrade
-sudo swapoff -a
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-
-sudo tee /etc/modules-load.d/containerd.conf <<EOF
-overlay
-br_netfilter
-EOF
- 
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
-sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
-EOF
-
-sudo sysctl --system
-
-
-
-
 sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
-
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
-
-
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
 sudo apt update
 sudo apt install -y containerd.io
 
