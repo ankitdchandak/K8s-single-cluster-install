7fc4d80f599327d3b900c74e5ab31590c7a8b597
Ankit Chandak <67820807+ankitdchandak@users.noreply.github.com>
2024-03-20 19:51:04 +0530

    Create containerd.sh

---
+++ containerd.sh
@@ -0,0 +1,5 @@
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+sudo apt-get update
